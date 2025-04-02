dcd4c42fd1ec688f58555befcc4476452f11ea41
Camille Bucatcat <camille.bucatcat@gmail.com>
2025-04-03 03:21:25 +0800

    update `primary color`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
   return (
     <>
-      <div className="bg-primary">
-        <p className="font-bold p-10">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, corrupti.</p>
+      <div className="bg-primary p-10">
+        <p className="font-bold">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, corrupti.</p>
       </div>
-      <div className="bg-primary-500">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, corrupti.</p>
+      <div className="bg-primary-50 p-10">
+        <p className="text-primary-800">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, corrupti.</p>
       </div>
     </>
   );
