f12e37b195264235d882e73a9965fdaf3cab689e
Camille Bucatcat <camille.bucatcat@gmail.com>
2025-04-03 03:24:25 +0800

    add `secondary color`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 export default function Home() {
   return (
     <>
+      <h1>Primary</h1>
       <div className="bg-primary p-10">
         <p className="font-bold">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, corrupti.</p>
       </div>
       <div className="bg-primary-50 p-10">
         <p className="text-primary-800">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, corrupti.</p>
+      </div>
+      <h1>Secondary</h1>
+      <div className="bg-secondary p-10">
+        <p className="font-bold">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, corrupti.</p>
+      </div>
+      <div className="bg-secondary-50 p-10">
+        <p className="text-secondary-800">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, corrupti.</p>
       </div>
     </>
   );
