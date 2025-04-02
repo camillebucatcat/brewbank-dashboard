4bb4a576a9c83be39173a5f57f7bb09cd97a9274
Camille Bucatcat <camille.bucatcat@gmail.com>
2025-04-03 03:37:46 +0800

    setup brand colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ export default function Home() {
       <div className="bg-primary p-10">
         <p className="font-bold">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, corrupti.</p>
       </div>
-      <div className="bg-primary-50 p-10">
+      <div className="bg-red-100 p-10">
         <p className="text-primary-800">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, corrupti.</p>
       </div>
       <h1>Secondary</h1>
-      <div className="bg-secondary p-10">
-        <p className="font-bold">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, corrupti.</p>
+      <div className="bg-red p-10">
+        <p className="font-bold text-success">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, corrupti.</p>
       </div>
       <div className="bg-secondary-50 p-10">
         <p className="text-secondary-800">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, corrupti.</p>
