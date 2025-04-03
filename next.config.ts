6927c061a9fbbcbad1d4dafeeddab7b7634e33db
renaemariz <61419385+renaemariz@users.noreply.github.com>
2025-04-04 04:18:06 +0800

    remove turbo on dev

--- next.config.ts
+++ next.config.ts
@@ -29,16 +29,6 @@ const nextConfig: NextConfig = {
 
     return config;
   },
-  experimental: {
-    turbo: {
-      rules: {
-        "*.svg": {
-          loaders: ["@svgr/webpack"],
-          as: "*.js",
-        },
-      },
-    },
-  },
 };
 
 export default nextConfig;
