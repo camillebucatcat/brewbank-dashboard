11866d7eaf519c33251474842876948a9c24a416
renaemariz <61419385+renaemariz@users.noreply.github.com>
2025-04-03 22:56:33 +0800

    setup svgr/webpack

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,34 @@
+import { Heart } from "@/assets/svg/index";
+
 export default function Home() {
   return (
     <>
+      <Heart height="30" width="30"></Heart>
       <h1>Primary</h1>
       <div className="bg-primary p-10">
-        <p className="font-bold">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, corrupti.</p>
+        <p className="font-bold">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio,
+          corrupti.
+        </p>
       </div>
       <div className="bg-red-100 p-10">
-        <p className="text-primary-800">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, corrupti.</p>
+        <p className="text-primary-800">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio,
+          corrupti.
+        </p>
       </div>
       <h1>Secondary</h1>
       <div className="bg-red p-10">
-        <p className="font-bold text-success">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, corrupti.</p>
+        <p className="font-bold text-success">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio,
+          corrupti.
+        </p>
       </div>
       <div className="bg-secondary-50 p-10">
-        <p className="text-secondary-800">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, corrupti.</p>
+        <p className="text-secondary-800">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio,
+          corrupti.
+        </p>
       </div>
     </>
   );
